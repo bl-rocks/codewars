d0098e2433c9ef2bd2886c88f33fa26e7f7016c1
Brian Laws <32749405+bl-rocks@users.noreply.github.com>
2024-08-13 14:46:59 -0400

    Update EnoughIsEnough.java
    changes to comments

--- EnoughIsEnough.java
+++ EnoughIsEnough.java
@@ -2,8 +2,10 @@ package code.laws;
 import java.util.*;
 /* Given a list lst and a number N, create a new list that contains each number of lst at 
  * most N times without reordering. For example if N = 2, and the input is [1,2,3,1,2,1,2,3], 
- * you take [1,2,3,1,2], drop the next [1,2] since this would lead to 1 and 2 being in the 
- * result 3 times, and then take 3, which leads to [1,2,3,1,2,3].
+ * the answer would be [1,2,1,2,] drop the next[1,2] since this would lead to 1 and 2 being in the 
+ * result 3  times which would be break initial requirements --> #times_in_result_array < N.
+ ###### UPDATED 8/15/24 BY: BRIAN LAWS
+ ###### only changes to comments made
  */
 
 public class EnoughIsEnough {
